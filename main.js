cb5c33890570a7d5262fdbfd0eb128730ee1f305
Fathan Sanum <134504162+Fansaa@users.noreply.github.com>
2025-02-26 15:11:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const navToggle = document.querySelector("nav ul");
+
     document.querySelector("header").addEventListener("click", function() {
         navToggle.classList.toggle("show");
     });
